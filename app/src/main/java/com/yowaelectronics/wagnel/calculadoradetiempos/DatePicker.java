263f401b39e6c0c42cb30b6119eacc60e27a4fa1
Wagnel <wagnelcuello@gmail.com>
2018-04-27 10:37:12 -0400

    Initial commit

---
+++ app/src/main/java/com/yowaelectronics/wagnel/calculadoradetiempos/DatePicker.java
@@ -0,0 +1,13 @@
+package com.yowaelectronics.wagnel.calculadoradetiempos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DatePicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_picker);
+    }
+}
